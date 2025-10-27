91716289c07a7748721e19aa012d66afe73da9b6
marchmi <marchmi@163.com>
2025-10-27 22:09:17 +0800

    【usage】supabase 增删改查

--- src/service/supabase.js
+++ src/service/supabase.js
@@ -1,4 +1,32 @@
 import { createClient } from '@supabase/supabase-js';
+import { errorMsg, createLoading } from '@/utils/interaction'
+ 
+let loadingInstance = null // loading 实例
+let loadingCount = 0 // 开起loading视图的请求的计数器
+
+/**
+ * 开启Loading
+ */
+function startLoading () {
+  if (loadingCount === 0) {
+    loadingInstance = createLoading()
+  }
+  loadingCount++
+}
+
+/**
+ * 关闭Loading
+ */
+function endLoading () {
+  setTimeout(() => {
+    if (loadingCount <= 0) return
+    loadingCount--
+    if (loadingCount === 0) {
+      loadingInstance.close ? loadingInstance.close() : ''
+    }
+  }, 200)
+}
+
 
 const supabaseUrl = 'https://jwzjobfpchmagrrgusrs.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3empvYmZwY2htYWdycmd1c3JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4NTczMjIsImV4cCI6MjA3NjQzMzMyMn0.gjhCcY6mkiwtwRrpXi1pLG8QJ2OUCRV7ZCteUhybCnU';
@@ -8,10 +36,12 @@ export const supabase = createClient(
     {
       global: {
         fetch: async (input, init) => {
+          console.log('input',input)
+          console.log('init',init)
+          startLoading()
           // —— 请求拦截 —— //
           const headers = new Headers(init?.headers)
           headers.set('x-lang', navigator.language)
-          headers.set('x-tenant', getTenantId())
   
           // —— 真正发请求 —— //
           const res = await fetch(input, { ...init, headers })
@@ -19,9 +49,10 @@ export const supabase = createClient(
           // —— 响应拦截 —— //
           if (!res.ok) {
             const body = await res.json()
-            ElMessage.error(body.message || 'Server error')
+            errorMsg(body.message || 'Server error')
             throw new Error(body.message)
           }
+          endLoading()
           return res
         }
       }
--- src/views/usage/supabase/Index.vue
+++ src/views/usage/supabase/Index.vue
@@ -7,18 +7,103 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import AppMain from '@/components/AppMain'
 import { supabase } from '@/service/supabase'
-async function getUsers() {
-    const { data, error } = await supabase
-        .from('users')
-        .select('*');
-    if (error) console.error(error);
-    else console.log(data);
+
+/**
+ * 查询表内的所有数据
+ * @param tableName 表名
+ */
+const fetchAll = async (tableName) => {
+    return new Promise( async (resolve, reject) => {
+        const { data: result, error } = await supabase
+            .from(tableName)
+            .select('*')
+        if (error) {
+            return reject(new Error(error))
+        }
+        resolve(result)
+    })
 }
 
-getUsers()
+const filter = reactive({
+    pageSize: 10,
+    currentPage: 1
+})
+
+const fetchList = async (params = {}, tableName) => {
+    const payload = { ...filter, ...params }
+    return new Promise( async (resolve, reject) => {
+        // 计算开始索引
+        const start = (payload.currentPage - 1) * payload.pageSize
+        // 计算结束索引
+        const end = start + ( payload.pageSize - 1);
+        let sqlQuery = supabase.from(tableName)
+            .select('*', { count: 'exact' })
+            .order('id', { ascending: true })
+            .range(start, end);
+
+        if (payload.username) {
+            sqlQuery = sqlQuery.like('username', `%${payload.username}%`)
+        }
+        if (payload.tel) {
+            sqlQuery = sqlQuery.eq('tel', payload.tel)
+        }
+
+        const { data: result, count, error } = await sqlQuery
+        console.log(result, count)
+        if (error) {
+            return reject(new Error(error))
+        }
+        resolve({
+            list: result,
+            total: count
+        })
+    })
+}
+
+const createOne = async (data, tableName) => {
+    return new Promise( async (resolve, reject) => {
+        const { data: result, error } = await supabase
+            .from(tableName)
+            .insert(data)
+        if (error) {
+            return reject(new Error(error))
+        }
+        resolve(result)
+    })
+}
+
+const updateOne = async (id, data, tableName) => {
+    return new Promise( async (resolve, reject) => {
+        const { data: result, error } = await supabase
+            .from(tableName)
+            .update(data)
+            .eq('id', id)
+        if (error) {
+            return reject(new Error(error))
+        }
+        resolve(result)
+    })
+}
+
+const deleteOne = async (id, tableName) => {
+    return new Promise( async (resolve, reject) => {
+        const { data: result, error } = await supabase
+            .from(tableName)
+            .delete()
+            .eq('id', id)
+        if (error) {
+            return reject(new Error(error))
+        }
+        resolve(result)
+    })
+}
+
+// createOne({username: '残虹1'}, 'users').then(res=>{
+//     console.log(res)
+// })
 </script>
 <style lang="less" scoped>
     
